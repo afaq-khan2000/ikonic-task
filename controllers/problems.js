62fbd80d9190e7e17e6c53aeb67d2a49fcc5160c
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2023-12-27 01:34:09 +0500

    final commit

--- controllers/problems.js
+++ controllers/problems.js
@@ -7,10 +7,7 @@ const directoryName = "../files";
 
 exports.problem_one = async function (req, res, next) {
   try {
-    const urls = req.body.urls;
-    if (!Array.isArray(urls)) {
-      throw new Error("Invalid input: expected an array of URLs");
-    }
+    const urls = ["https://api.agify.io/?name=afaq", "https://www.boredapi.com/api/activity", "http://universities.hipolabs.com/search?country=Pakistan", "https://datausa.io/api/data?drilldowns=Nation&measures=Population"];
     const downloadPromises = urls.map((url) => axios.get(url).then((response) => response.data));
     const contents = await Promise.all(downloadPromises);
     res.status(200).json(contents);
@@ -29,9 +26,19 @@ exports.problem_two = async function (req, res, next) {
       url: "https://api.publicapis.org/entries",
     };
     const response = await axios.request(config);
-    res.status(200).json(response.data);
+    if (response.status === 200) {
+      res.status(200).json(response.data);
+    } else {
+      res.status(response.status).json({ message: `Server responded with status code ${response.status}` });
+    }
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    if (e.response.status === 404) {
+      res.status(404).json({ message: "Not found" });
+    } else if (e.response.status === 403) {
+      res.status(403).json({ message: "Forbidden" });
+    } else {
+      res.status(500).json({ message: e.message });
+    }
   }
 };
 
@@ -42,6 +49,7 @@ exports.problem_three = async function (req, res, next) {
     const textFiles = files.filter((file) => file.endsWith(".txt"));
     const contents = [];
     for (const file of textFiles) {
+      console.log("reading file: ", file);
       const content = fs.readFileSync(path.join(directoryPath, file), "utf8");
       contents.push(content);
     }
